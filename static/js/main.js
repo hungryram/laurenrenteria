0ac0ef70b204801027ce7716199de4e79b789d66
Ram Dettmer <ramdettmer751@gmail.com>
2020-12-20 20:40:06 -0800

    updates

--- static/js/main.js
+++ static/js/main.js
@@ -36,3 +36,42 @@ $('.responsive-slide').slick({
       // instead of a settings object
     ]
   });
+
+  $('.product-slide').slick({
+    dots: true,
+    infinite: true,
+    speed: 2000,
+    autoplaySpeed: 3000,
+    slidesToShow: 1,
+    autoplay: true,
+    slidesToScroll: 1,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
