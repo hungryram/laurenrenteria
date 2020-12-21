4bb69e1b96675195b8613f66dd0feeb3a6c39b0f
Ram Dettmer <ramdettmer751@gmail.com>
2020-12-21 09:02:53 -0800

    updates

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,31 @@
+// MENU
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
+
+
 $('.responsive-slide').slick({
     dots: true,
     infinite: true,
@@ -5,6 +33,7 @@ $('.responsive-slide').slick({
     autoplaySpeed: 3000,
     slidesToShow: 3,
     autoplay: true,
+    arrows: false,
     slidesToScroll: 4,
     pauseOnHover: true,
     responsive: [
@@ -38,14 +67,17 @@ $('.responsive-slide').slick({
   });
 
   $('.product-slide').slick({
-    dots: true,
     infinite: true,
-    speed: 2000,
+    speed: 1000,
+    adaptiveHeight: true,
     autoplaySpeed: 3000,
     slidesToShow: 1,
     autoplay: true,
     slidesToScroll: 1,
     pauseOnHover: true,
+    arrows: true,
+    prevArrow:"<button type='button' class='slick-prev pull-left'><img src='/assets/chevron-left.svg' width='20' title='previous'></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><img src='/assets/chevron-right.svg' width='20' title='next'></button>",
     responsive: [
       {
         breakpoint: 1024,
